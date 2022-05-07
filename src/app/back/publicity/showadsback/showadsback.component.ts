828346d4793654ce64fd05de3acab1f260046eeb
shidono <shidonosan@gmail.com>
2022-05-08 00:36:52 +0100

    trying to fix buttons + pagination backend

--- src/app/back/publicity/showadsback/showadsback.component.ts
+++ src/app/back/publicity/showadsback/showadsback.component.ts
@@ -1,24 +1,30 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { AcceuilService } from 'src/app/services/acceuil.service';
 import Swal from 'sweetalert2';
 import { Chart } from 'node_modules/chart.js';
+import { DOCUMENT } from '@angular/common';
 import { Waves } from '../../../../assets/js/waves.js';
-
+// var Waves = require('node-waves');
 declare var $: any;
 @Component({
   selector: 'app-showaddback',
   templateUrl: './showadsback.component.html',
   styleUrls: ['./showadsback.component.css'],
 })
-export class ShowadsbackComponent implements OnInit,AfterViewInit {
+export class ShowadsbackComponent implements OnInit, AfterViewInit {
   message: any;
   type: string = 'Remise';
   region: string = 'Global';
   place?: string;
   stats: any;
   listpub: any;
-
-  constructor(private service: AcceuilService) {}
+  window: any = window;
+  constructor(
+    private service: AcceuilService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.window = this.document.defaultView;
+  }
   ngAfterViewInit(): void {
     scriptReady();
   }
@@ -26,6 +32,8 @@ export class ShowadsbackComponent implements OnInit,AfterViewInit {
     this.listAd();
     this.getStats();
     scriptInit();
+    // alert(this.window.);
+    // this.window['Waves'].init();
   }
   public changeChart(datas) {
     console.log('chart is : ' + datas);
@@ -142,34 +150,115 @@ export class ShowadsbackComponent implements OnInit,AfterViewInit {
   }
 }
 
-
-function scriptReady(){
-  $("#basic-datatable").DataTable({ language: { paginate: { previous: "<i class='mdi mdi-chevron-left'>", next: "<i class='mdi mdi-chevron-right'>" } }, drawCallback: function() { $(".dataTables_paginate > .pagination").addClass("pagination-rounded") } });
-  var a = $("#datatable-buttons").DataTable({ lengthChange: !1, buttons: ["copy", "print", "pdf"], language: { paginate: { previous: "<i class='mdi mdi-chevron-left'>", next: "<i class='mdi mdi-chevron-right'>" } }, drawCallback: function() { $(".dataTables_paginate > .pagination").addClass("pagination-rounded") } });
-  $("#selection-datatable").DataTable({ select: { style: "multi" }, language: { paginate: { previous: "<i class='mdi mdi-chevron-left'>", next: "<i class='mdi mdi-chevron-right'>" } }, drawCallback: function() { $(".dataTables_paginate > .pagination").addClass("pagination-rounded") } }), $("#key-datatable").DataTable({ keys: !0, language: { paginate: { previous: "<i class='mdi mdi-chevron-left'>", next: "<i class='mdi mdi-chevron-right'>" } }, drawCallback: function() { $(".dataTables_paginate > .pagination").addClass("pagination-rounded") } }), a.buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"), $("#complex-header-datatable").DataTable({ language: { paginate: { previous: "<i class='mdi mdi-chevron-left'>", next: "<i class='mdi mdi-chevron-right'>" } }, drawCallback: function() { $(".dataTables_paginate > .pagination").addClass("pagination-rounded") }, columnDefs: [{ visible: !1, targets: -1 }] }), $("#state-saving-datatable").DataTable({ stateSave: !0, language: { paginate: { previous: "<i class='mdi mdi-chevron-left'>", next: "<i class='mdi mdi-chevron-right'>" } }, drawCallback: function() { $(".dataTables_paginate > .pagination").addClass("pagination-rounded") } })
+function scriptReady() {
+  $('#basic-datatable').DataTable({
+    language: {
+      paginate: {
+        previous: "<i class='mdi mdi-chevron-left'>",
+        next: "<i class='mdi mdi-chevron-right'>",
+      },
+    },
+    drawCallback: function () {
+      $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+    },
+  });
+  var a = $('#datatable-buttons').DataTable({
+    lengthChange: !1,
+    buttons: ['copy', 'print', 'pdf'],
+    language: {
+      paginate: {
+        previous: "<i class='mdi mdi-chevron-left'>",
+        next: "<i class='mdi mdi-chevron-right'>",
+      },
+    },
+    drawCallback: function () {
+      $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+    },
+  });
+  $('#selection-datatable').DataTable({
+    select: { style: 'multi' },
+    language: {
+      paginate: {
+        previous: "<i class='mdi mdi-chevron-left'>",
+        next: "<i class='mdi mdi-chevron-right'>",
+      },
+    },
+    drawCallback: function () {
+      $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+    },
+  }),
+    $('#key-datatable').DataTable({
+      keys: !0,
+      language: {
+        paginate: {
+          previous: "<i class='mdi mdi-chevron-left'>",
+          next: "<i class='mdi mdi-chevron-right'>",
+        },
+      },
+      drawCallback: function () {
+        $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+      },
+    }),
+    a
+      .buttons()
+      .container()
+      .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)'),
+    $('#complex-header-datatable').DataTable({
+      language: {
+        paginate: {
+          previous: "<i class='mdi mdi-chevron-left'>",
+          next: "<i class='mdi mdi-chevron-right'>",
+        },
+      },
+      drawCallback: function () {
+        $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+      },
+      columnDefs: [{ visible: !1, targets: -1 }],
+    }),
+    $('#state-saving-datatable').DataTable({
+      stateSave: !0,
+      language: {
+        paginate: {
+          previous: "<i class='mdi mdi-chevron-left'>",
+          next: "<i class='mdi mdi-chevron-right'>",
+        },
+      },
+      drawCallback: function () {
+        $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+      },
+    });
 }
 
-function scriptInit(){
-  $("#side-menu").metisMenu(), $("#vertical-menu-btn").on("click", function() { 
-      $("body").toggleClass("enable-vertical-menu") 
-  }), 
-  $(".menu-overlay").on("click", function() { 
-    $("body").removeClass("enable-vertical-menu")
-  }), 
-  $("#sidebar-menu a").each((i,element) => {
-    var a = window.location.href.split(/[?#]/)[0];
-    element.href == a && ($(element).addClass("active"), 
-    $(element).paren$().addClass("mm-active"), 
-    $(element).paren$().paren$().addClass("mm-show"), 
-    $(element).paren$().paren$().prev().addClass("mm-active"), 
-    $(element).paren$().paren$().paren$().addClass("mm-active"), 
-    $(element).paren$().paren$().paren$().paren$().addClass("mm-show"),
-    $(element).paren$().paren$().paren$().paren$().paren$().addClass("mm-active"))
-  }), $(function() { 
-    $('[data-toggle="tooltip"]').tooltip() 
-  }), 
-  $(function() { 
-      $('[data-toggle="popover"]').popover() 
-  }),
-  Waves.init()
+function scriptInit() {
+  $('#side-menu').metisMenu(),
+    $('#vertical-menu-btn').on('click', function () {
+      $('body').toggleClass('enable-vertical-menu');
+    }),
+    $('.menu-overlay').on('click', function () {
+      $('body').removeClass('enable-vertical-menu');
+    }),
+    $('#sidebar-menu a').each((i, element) => {
+      var a = window.location.href.split(/[?#]/)[0];
+      element.href == a &&
+        ($(element).addClass('active'),
+        $(element).paren$().addClass('mm-active'),
+        $(element).paren$().paren$().addClass('mm-show'),
+        $(element).paren$().paren$().prev().addClass('mm-active'),
+        $(element).paren$().paren$().paren$().addClass('mm-active'),
+        $(element).paren$().paren$().paren$().paren$().addClass('mm-show'),
+        $(element)
+          .paren$()
+          .paren$()
+          .paren$()
+          .paren$()
+          .paren$()
+          .addClass('mm-active'));
+    }),
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+    }),
+    $(function () {
+      $('[data-toggle="popover"]').popover();
+    });
+  Waves.init();
 }
